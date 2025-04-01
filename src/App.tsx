b60306cac75088e9dba86cfc35c3f443e2ea88e2
Luanaap <57630311+Luanaap@users.noreply.github.com>
2025-03-31 21:02:07 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components' 
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+import { Router } from './Routes'
+import { BrowserRouter } from 'react-router-dom'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+      <GlobalStyle/>
+    </ThemeProvider>
+  )
+}
