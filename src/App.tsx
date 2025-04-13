26ad146880e2c7cf6185dcbbaf7cefd1e87e49a1
Luana Andrade <57630311+Luanaap@users.noreply.github.com>
2025-04-13 19:26:37 -0300

    feat: add hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 import { Router } from './Routes'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CyclesContextProvider } from './context/CyclesContext'
 
 
@@ -10,11 +10,11 @@ export function App() {
   
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <HashRouter>
       <CyclesContextProvider>
         <Router/>
       </CyclesContextProvider>
-      </BrowserRouter>
+      </HashRouter>
       <GlobalStyle/>
     </ThemeProvider>
   )
